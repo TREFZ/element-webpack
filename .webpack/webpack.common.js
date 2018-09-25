6948d7d80bdcd396e20d992148cfe3b73e30ed5a
Nick Trefz <nick_trefz@timeinc.com>
2018-09-25 12:03:14 -0400

    Get production builds working

--- .webpack/webpack.common.js
+++ .webpack/webpack.common.js
@@ -1,8 +1,130 @@
+const webpack = require('webpack');
+const path = require('path');
+const fs = require('fs');
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const autoprefixer = require('autoprefixer');
+
 module.exports = {
   entry: {
-    main: './index.js',
+    main: [path.join(__dirname, '..', '..', 'element', 'public/src/main.js')],
   },
   output: {
-    filename: 'main.js',
+    path: path.resolve('./public/dist/'),
+    publicPath: '/dist/',
+    filename: '[name].js',
+    chunkFilename: '[name]-[id].js',
+  },
+  resolve: {
+    alias: {
+      public: path.join(__dirname, '..', '..', 'element', 'public'),
+      shared: path.join(__dirname, '..', '..', 'element', 'shared'),
+      brandPublic: path.join(__dirname, '..', '..', 'element', 'public'),
+      brandShared: path.join(__dirname, '..', '..', 'element', 'shared'),
+      handlebars: 'handlebars/runtime.js',
+    },
+    extensions: ['.js', '.coffee'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.coffee$/,
+        use: 'coffee-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'resolve-url-loader',
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: () => [autoprefixer()],
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|svg)$/,
+        include: path.join(__dirname, '..', '..', 'element', 'public'),
+        use: 'url-loader?limit=30000&name=images/[name].[ext]',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules(?!\/element)/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['env', { modules: false }],
+          ],
+        },
+      },
+      {
+        test: /\.hbs/,
+        loader: 'handlebars-loader',
+        query: {
+          exclude: 'node_modules',
+          partialResolver: (partial, callback) => {
+            const partialPath = `${path.join(__dirname, '..', '..', 'element', 'shared', 'partials/')}${partial}.hbs`;
+            const componentPath = `${path.join(__dirname, '..', '..', 'element', 'shared', 'components/')}${partial}.hbs`;
+            fs.stat(partialPath, (err, stat) => {
+              if (err === null && stat) {
+                callback(null, partialPath);
+              } else {
+                callback(null, componentPath);
+              }
+            });
+          },
+          helperResolver: (helper, callback) => {
+            const helperPath = `${path.join(__dirname, '..', '..', 'element', 'shared', 'helpers/')}${helper.replace('./', '')}.js`;
+            fs.stat(helperPath, (err, stat) => {
+              if (err === null && stat) {
+                callback(null, helperPath);
+              } else {
+                callback();
+              }
+            });
+          },
+        },
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true,
+        },
+      },
+    },
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: '[name].css' }),
+    new LodashModuleReplacementPlugin({
+      paths: true,
+      caching: true,
+      cloning: true,
+    }),
+    new webpack.optimize.MinChunkSizePlugin({ minChunkSize: 10000 }),
+  ],
+  node: {
+    fs: 'empty',
   },
 };
---
+++ bin/element-webpack.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+// import webpack from 'webpack';
+
+const [,, ...args] = process.argv;
+
+console.log(`Hello world, I am an executable script ${args}`);
